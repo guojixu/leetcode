3f451948f767b777f65fa38556bdd9b16ce3d106
ji-xu-guo <765452843@qq.com>
2018-07-08 15:53:40 +0800

    新增:Maximum of binary tree 新增:Inbert Binary Tree

---
+++ src/main/java/com/guoxu/maxmumofbinarytree/MaxDepth.java
@@ -0,0 +1,111 @@
+package com.guoxu.maxmumofbinarytree;
+
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Solution {
+    public int maxDepth(TreeNode root) {
+        int leftDepth = 0;
+        int rightDepth = 0;
+        if (root == null) {
+            return 0;
+        }
+        leftDepth = 1 + maxDepth(root.left);
+        rightDepth = 1 + maxDepth(root.right);
+        if (leftDepth >= rightDepth){
+            return leftDepth;
+        } else {
+            return rightDepth;
+        }
+    }
+}
+
+public class MaxDepth {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            int ret = new Solution().maxDepth(root);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
