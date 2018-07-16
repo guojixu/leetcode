51aadbd752f2c8208a7830ae94374814e8e31a1b
ji-xu-guo <765452843@qq.com>
2018-07-16 20:42:19 +0800

    新增：BinaryTreeLevelOrderTraversal

---
+++ src/main/java/com/guoxu/binarytreelevelordertraversal/MainClass.java
@@ -0,0 +1,187 @@
+package com.guoxu.binarytreelevelordertraversal;
+
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+import sun.reflect.generics.tree.Tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {val = x;}
+}
+class Solution {
+    /**
+     * 参考讨论区高票答案
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+
+        if (root == null) {
+            return wrapList;
+        }
+
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for (int i = 0;i < levelNum;i++) {
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+//    public List<List<Integer>> levelOrder(TreeNode root) {
+//        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+//        if (root == null) {
+//            return ans;
+//        }
+//        queue.add(root);
+//        TreeNode last = root;
+//        TreeNode nlast = root;
+//        List<Integer> temp = new LinkedList<Integer>();
+//        while (!queue.isEmpty()) {
+//            TreeNode t = queue.poll();
+//            temp.add(t.val);
+//            if (t.left != null) {
+//                queue.add(t.left);
+//                nlast = t.left;
+//            }
+//            if (t.right != null) {
+//                queue.add(t.right);
+//                nlast = t.right;
+//            }
+//            if (last == t) {
+//                ans.add(temp);
+//                last = nlast;
+//                temp = new LinkedList<Integer>();
+//            }
+//        }
+//        return ans;
+//    }
+
+}
+
+public class MainClass {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            List<List<Integer>> ret = new Solution().levelOrder(root);
+
+            String out = int2dListToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
