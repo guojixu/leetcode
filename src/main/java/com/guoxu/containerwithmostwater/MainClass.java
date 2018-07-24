8430f8d875d8d4ccade95d3756232a5e6d069317
ji-xu-guo <765452843@qq.com>
2018-07-24 15:50:42 +0800

    新增：ContainerWithMostWater 新增：NumberOfIsland

---
+++ src/main/java/com/guoxu/containerwithmostwater/MainClass.java
@@ -0,0 +1,53 @@
+package com.guoxu.containerwithmostwater;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            }
+            else {
+                r--;
+            }
+        }
+        return maxarea;
+    }
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] height = stringToIntegerArray(line);
+
+            int ret = new Solution().maxArea(height);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
