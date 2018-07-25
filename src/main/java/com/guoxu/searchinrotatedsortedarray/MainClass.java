56b0adf8ee6daba3840c437a9cb0774a9ae47b1d
ji-xu-guo <765452843@qq.com>
2018-07-25 17:10:33 +0800

    新增：SearchInRotatedSortedArray

---
+++ src/main/java/com/guoxu/searchinrotatedsortedarray/MainClass.java
@@ -0,0 +1,50 @@
+package com.guoxu.searchinrotatedsortedarray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i ++)
+        {
+            if(nums[i] == target)
+                return i;
+        }
+        return -1;
+    }
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+            line = in.readLine();
+            int target = Integer.parseInt(line);
+
+            int ret = new Solution().search(nums, target);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
